223a3616563a82bded5f4e9e2e77313401611ed0
SNORLAX7355 <philliot1304@gmail.com>
2026-05-05 18:55:32 -0700

    explore completed

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
-Eliot Yoo
+Member: Eliot Yoo
+
+https://snorlax7355.github.io/Lab5_Starter/expose.html
+
+https://snorlax7355.github.io/Lab5_Starter/explore.html
 
 1. No, you wouldn't use a unit test to test the "message" feature of a message application since this is a larger scale feature that interacts with many elements of the program. A unit test would fail to test how the individual components of the application (such as type, send, recieve, etc) interact with each other on a application/feature level.
 2. Yes, you would use a unit test to test the "max message length" feature of a messaging application since it is small scale feature that limits one aspect of the applitcation. It can executed and tested quickly to debug without taking too much time and effort.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,26 +1,57 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-const synth = window.speechSynthesis;
-let voices = [];
+let synth;
+let voiceSelect;
+let textArea;
+let button;
+let faceImage;
 
 function init() {
+  synth = window.speechSynthesis;
+  voiceSelect = document.getElementById('voice-select');
+  textArea = document.getElementById('text-to-speak');
+  button = document.querySelector('button');
+  faceImage = document.querySelector('#explore img');
+
   populateVoiceList();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  button.addEventListener('click', handleSpeak);
 }
 
 function populateVoiceList() {
-  const voiceSelect = document.querySelector('select');
-  voices = synth.getVoices();
+  const voices = synth.getVoices();
+  voiceSelect.innerHTML = '<option disabled selected>Select Voice:</option>'
 
-  for (const voice in voices) {
+  voices.forEach((voice, index) => {
     const option = document.createElement('option');
+    option.value = index;
     option.textContent = `${voice.name} (${voice.lang})`;
-    
-    if (voice.default) {
-      option.textContent += ' - DEFAULT';
-    }
-
-    option.setAttribute('value', voice.name);
     voiceSelect.appendChild(option);
+  });
+}
+
+function handleSpeak() {
+  const text = textArea.value;
+  const voices = synth.getVoices();
+  const index = voiceSelect.value;
+
+  if (!text || index === '' || index === 'select') return;
+
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.voice = voices[index];
+
+  utterance.onstart = () => {
+    faceImage.src = 'assets/images/smiling-open.png'
   }
+
+  utterance.onend = () => {
+    faceImage.src = 'assets/images/smiling.png'
+  }
+
+  synth.speak(utterance);
 }
